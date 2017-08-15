bad5d4f9bdfa1fae02cb4072c5ed0d717ee7daa8
Adith <adithwidyapradipta@gmail.com>
2017-08-15 14:48:38 +0700

    routing

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var usercont = require('../controllers/usercont')
+var articlecont = require('../controllers/articlecont')
+
+router.post('/signup', usercont.signup)
+router.post('/login', usercont.login)
+router.post('/userdata', usercont.userData)
+
+router.get('/api/articles', articlecont.find)
+router.get('/api/articles/:id', articlecont.findOne)
+router.post('/api/articles', usercont.userInfo, articlecont.create)
+router.put('/api/articles/:id', usercont.userInfo, articlecont.update)
+router.delete('/api/articles/:id', articlecont.remove)
 
 module.exports = router;
